5832bcccf015e264010e7990a872e72c31a2e598
Alessandro Pasotti <elpaso@itopen.it>
2021-06-04 16:01:44 +0200

    Fix update_localita save error

--- archive.sh
+++ archive.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# Crea il pacchetto zip dal branch specificato in riga di comando (default: master) 
+# Crea il pacchetto zip dal branch specificato in riga di comando (default: master)
 
-BRANCH=${1:-master}
+BRANCH=${1:-qgis3-port-bugfix}
 
 git archive --prefix cle-tools/ --format=zip -o cle-tool-qgis3.zip ${BRANCH}
 
